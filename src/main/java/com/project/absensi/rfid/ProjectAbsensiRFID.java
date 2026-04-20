6e08d5dc23eca386042e669f120692c248f4e55a
Fatih_31 <fatihmubarok850@gmail.com>
2026-04-20 20:40:04 +0700

    Test

---
+++ src/main/java/com/project/absensi/rfid/ProjectAbsensiRFID.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.project.absensi.rfid;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProjectAbsensiRFID {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
