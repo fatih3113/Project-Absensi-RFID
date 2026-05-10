ed9dfd38e1f5451f226a3214adaf043934e316c4
Fatih_31 <fatihmubarok850@gmail.com>
2026-05-10 08:46:33 +0700

    Halaman Login Admin

--- src/main/java/com/project/absensi/rfid/GUI/HalamanLoginAdmin.java
+++ src/main/java/com/project/absensi/rfid/GUI/HalamanLoginAdmin.java
@@ -10,12 +10,12 @@ import com.project.absensi.rfid.service.AuthService;
  *
  * @author ACER
  */
-public class HalamanLogin extends javax.swing.JFrame {
+public class HalamanLoginAdmin extends javax.swing.JFrame {
 
     /**
      * Creates new form HalamanLogin
      */
-    public HalamanLogin() {
+    public HalamanLoginAdmin() {
         initComponents();   
         setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
 
@@ -255,20 +255,21 @@ public class HalamanLogin extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HalamanLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HalamanLoginAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HalamanLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HalamanLoginAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HalamanLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HalamanLoginAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HalamanLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HalamanLoginAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new HalamanLogin().setVisible(true);
+                new HalamanLoginAdmin().setVisible(true);
             }
         });
     }
